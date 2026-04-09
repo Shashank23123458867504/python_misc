be8df504c45265414b975202894472cfa57beb36
Shashank767 <143302681+Shashank23123458867504@users.noreply.github.com>
2026-04-09 10:36:24 +0530

    Improve prime number check logic
    Added a check for numbers less than or equal to 1.

--- prime.py
+++ prime.py
@@ -1,8 +1,11 @@
 n = int(input())
-for i in range(2, n//2 + 1):
-    if n%i==0:
-        print("Not a Prime Number")
-        break
+if n<=1:
+    print("Not a Prime Number")
 else:
-    print("It is a Prime Number")
+    for i in range(2, n//2 + 1):
+        if n%i==0:
+            print("Not a Prime Number")
+            break
+    else:
+        print("It is a Prime Number")
     
