509a8d4923e9154255d4de8c36f6986939aa924b
Shashank767 <143302681+Shashank23123458867504@users.noreply.github.com>
2026-04-09 10:38:01 +0530

    Improve prime number checking algorithm
    Optimize prime number check to use square root method.

--- prime.py
+++ prime.py
@@ -2,7 +2,7 @@ n = int(input())
 if n<=1:
     print("Not a Prime Number")
 else:
-    for i in range(2, n//2 + 1):
+    for i in range(2, int(n ** 0.5) + 1):
         if n%i==0:
             print("Not a Prime Number")
             break
